29117c93e858f45cd8d1df7f9fc4059aef026ce7
Akshata Rao <akshatakrao@gmail.com>
2014-01-31 19:46:12 -0500

    Update 2: AOS-Assgt 1

--- aos/assgt_1/gtthread.c
+++ aos/assgt_1/gtthread.c
@@ -119,6 +119,120 @@ gtthread_t* getRunningThread()
 }
 
 
+/**
+ *
+ */
+int gtthread_yield()
+{
+
+    //Get currently running thread
+    gtthread_node* runningThread = getRunningThread();
+    gtthread_node* nextThread = NULL;
+
+    //Check if running thread exists
+    if(runningThread == NULL)
+    {
+        fprintf(stderr, "No thread found to be running right now!");
+        return 1;
+    }
+
+   nextThread = removeThreadFromQueue(&readyQueue);
+
+   //If another thread exists in the ready queue, then
+   //swap contexts
+   
+   if(nextThread == NULL)
+   {
+        fprintf(stderr, "No other thread found in the ready queue");
+        return 0;
+   }
+
+   runningThread->state = READY;
+   addThreadToQueue(runningThread);
+
+   nextThread->state = RUNNING;
+   swapcontext(&(runningThread->context), &(nextThread->context));
+
+    //Push thread to back of queue
+    //Bring the next thread to front of queue
+    //Do a swap of context
+
+   return 0;
+}
+
+/**
+ *
+ */
+void gtthread_exit(void* retval)
+{
+  gtthread_t* runningThread = NULL, *nextThread = NULL;
+  gtthread_node* ptrToChildren = NULL, *nextThreadNode = NULL;
+  int returnVal = 0;
+
+  //Get Running Thread
+  runningThread = getRunningThread();
+
+  if(runningThread == NULL)
+  {
+      fprintf(stderr, "\nNo running thread was found.");
+      exit(1);
+  }
+
+  //Change status of running thread
+  runningThread->state = TERMINATED;
+  ptrToChildren = runningThread->childthreads;
+
+  //Terminate the child threads recursively
+  while(ptrToChildren != NULL)
+  {
+    killChildThreads(ptrToChildren);
+    ptrToChildren = ptrToChildren->link;
+  }
+
+  nextThreadNode = removeThreadFromQueue(&readyQueue); 
+
+  if(nextThreadNode != NULL)
+  {
+    nextThread = nextThreadNode->thread;
+    swapcontext(&runningThread->context, &nextThread->context);
+  }
+  
+  //TODO: Set return value in retval
+
+  retval = (void *)&returnVal;
+}
+
+/**
+ * Kill Child Threads
+ */
+void killChildThreads(gtthread_node* threadNode)
+{
+    
+  gtthread_t* thread = threadNode->thread;  
+  gtthread_node* childthreads = thread->childthreads;
+
+  thread->state = TERMINATED;
+
+  while(childthreads != NULL)
+  {
+
+      if(childthreads->thread->state != TERMINATED)
+            killChildThreads(childthreads);
+      childthreads = childthreads->link;
+  }
+}
+  
+/**
+ * Check if threads are equal
+ */     
+int  gtthread_equal(gtthread_t t1, gtthread_t t2)
+{
+
+    if((t1 != NULL && t2 != NULL) && (t1->threadID == t2->threadID))
+    {
+       return 1;
+    }
+}
 
 int main()
 {
